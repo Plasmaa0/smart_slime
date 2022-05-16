ed5ff5e128508622db7a128ba050ac79e6c0d546
Plasmaa0 <plasmaa0@gmail.com>
2022-05-16 12:02:26 +0300

    ball class

--- include/Ball.h
+++ include/Ball.h
@@ -1,14 +1,19 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Ball
+#include "Entity.h"
+
+class Ball : public Entity
 {
    private:
 	sf::Vector2f m_position;
 	sf::Vector2f m_velocity;
+	sf::Vector2f m_acceleration;
 	float m_radius;
 
    public:
+	friend class Game;
 	Ball(float px, float py, float radius);
-	void draw(sf::RenderTarget &pTarget);
+	void draw(sf::RenderTarget &pTarget) const override;
+	void update(sf::Time deltatime);
 };
--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,14 +1,24 @@
 #include "Ball.h"
 
 Ball::Ball(float px, float py, float radius)
-    : m_position(px, py), m_velocity(0, 0), m_radius(radius)
+    : m_position(px, py),
+      m_velocity(0, 0),
+      m_acceleration(0, 0),
+      m_radius(radius)
 {
 }
 
-void Ball::draw(sf::RenderTarget &pTarget)
+void Ball::draw(sf::RenderTarget &pTarget) const
 {
 	sf::CircleShape circle;
 	circle.setRadius(m_radius);
 	circle.setPosition(m_position);
 	pTarget.draw(circle);
+}
+
+void Ball::update(sf::Time deltatime)
+{
+	m_velocity +=
+	    (m_acceleration + sf::Vector2f(0, 600)) * deltatime.asSeconds();
+	m_position += m_velocity * deltatime.asSeconds();
 }
