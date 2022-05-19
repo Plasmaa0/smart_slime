26c994f6dd0272619ab5f119cb1ccb72a523d24f
Plasmaa0 <plasmaa0@gmail.com>
2022-05-19 11:52:51 +0300

    disabled debug output

--- src/Player.cpp
+++ src/Player.cpp
@@ -24,24 +24,24 @@ void Player::update(sf::Time deltatime)
 {
 	if (movingLeft) {
 		movingLeft = false;
-		printf("movingLeft\n");
+		// printf("movingLeft\n");
 		m_velocity.x = -m_radius * 10;
 	}
 	if (movingRight) {
 		movingRight = false;
-		printf("movingRight\n");
+		// printf("movingRight\n");
 		m_velocity.x = m_radius * 10;
 	}
 	if (jumping) {
 		jumping = false;
-		printf("jumping\n");
+		// printf("jumping\n");
 		m_velocity.y = -m_radius * 22;
 	}
 	// m_acceleration.y += 10;
 	// if (m_acceleration.y > 1000) m_acceleration.y = 1000;
 	auto summ_acceleration = m_acceleration + sf::Vector2f(0, 3000);
 	m_velocity += summ_acceleration * deltatime.asSeconds();
-	printf("%f, %f\n", summ_acceleration.x, summ_acceleration.y);
+	// printf("%f, %f\n", summ_acceleration.x, summ_acceleration.y);
 	m_position += m_velocity * deltatime.asSeconds();
 	m_acceleration.x *= 0.0f;
 	m_velocity.x *= 0.9f;
